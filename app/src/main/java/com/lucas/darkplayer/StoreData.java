60e36526c270c17c8c302e0ca75802a64872c644
Lucas Lee <lucasleeeeeeeee@gmail.com>
2019-09-13 13:32:16 +0800

    StoreData: remove unused functions and add function to store repeat.
    repeat is should be loaded on start as a user preference as I would consider it part of setting playback behaviour. It's also easier to set it and retrieve it in different fragments should we need it.
    
    Signed-off-by: Lucas Lee <lucasleeeeeeeee@gmail.com>

--- app/src/main/java/com/lucas/darkplayer/StoreData.java
+++ app/src/main/java/com/lucas/darkplayer/StoreData.java
@@ -21,71 +21,16 @@ public class StoreData {
         this.context = context;
     }
 
-    public void storeAudioData(ArrayList<SongData> arrayList){
+    public boolean loadRepeat(){
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        return preferences.getBoolean("repeat", false);
+    }
+
+    public void storeRepeat(boolean repeat){
         preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(arrayList);
-        editor.putString("songArrayList", json);
+        editor.putBoolean("repeat", repeat);
         editor.apply();
-
-    }
-    public boolean loadShuffled(){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        return preferences.getBoolean("shuffled", false);
-    }
-
-    public ArrayList<SongData> loadAudio() {
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = preferences.getString("songData", null);
-        Type type = new TypeToken<ArrayList<SongData>>(){
-        }.getType();
-        return gson.fromJson(json, type);
-    }
-
-    public void storeAudioIndex(int index){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt("audioIndex", index);
-        editor.apply();
-    }
-
-    public void storeShuffled(boolean shuffled){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("shuffled", shuffled);
-        editor.apply();
-    }
-
-    public void storeAudioShuffleList(int[] shuffleList){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(shuffleList);
-        editor.putString("shuffleList", json);
-        editor.apply();
-    }
-
-    public int[] loadShuffleList() {
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = preferences.getString("shuffleList", null);
-        Type type = new TypeToken<int[]>(){
-        }.getType();
-        return gson.fromJson(json, type);
-    }
-
-    public void storeSession(Boolean playing){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("playing", playing);
-        editor.apply();
-    }
-
-    public int loadAudioIndex(){
-        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
-        return preferences.getInt("audioIndex", -1);
     }
 
     public void clearCached(){
